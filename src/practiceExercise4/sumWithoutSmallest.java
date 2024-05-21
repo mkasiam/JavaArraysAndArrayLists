69e19ecfe8813371a51468decb56656723090161
Kawsar Ahmmed <kawsarahmmed10001@gmail.com>
2024-05-21 11:04:08 +0600

    Sum Without Smallest Code done

--- src/practiceExercise4/sumWithoutSmallest.java
+++ src/practiceExercise4/sumWithoutSmallest.java
@@ -12,7 +12,24 @@ public class sumWithoutSmallest {
             numbers[i] = random.nextInt();
         }
 
+        int sum = 0;
+        int smallest = numbers[0];
 
-        for (int i = 0)
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] < smallest) {
+                smallest = numbers[i];
+            }
+        }
+
+        boolean smallestExcluded = false;
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] == smallest && !smallestExcluded) {
+                smallestExcluded = true;
+            } else {
+                sum += numbers[i];
+            }
+        }
+        System.out.println(sum);
     }
 }
