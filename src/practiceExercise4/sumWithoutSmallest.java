a5cd3f628c1bbfec8b78a4c073882b307a5ba085
Kawsar Ahmmed <kawsarahmmed10001@gmail.com>
2024-05-22 09:37:41 +0600

    Sum Without Smallest Code done in different way

--- src/practiceExercise4/sumWithoutSmallest.java
+++ src/practiceExercise4/sumWithoutSmallest.java
@@ -6,30 +6,30 @@ public class sumWithoutSmallest {
     public static void main(String[] args) {
         final int LENGTH = 10;
         int[] numbers = new int[LENGTH];
-        Random random = new Random(100);
+        Random random = new Random();
+
 
         for (int i = 0; i < numbers.length; i++) {
-            numbers[i] = random.nextInt();
+            numbers[i] = random.nextInt(100);
         }
 
         int sum = 0;
         int smallest = numbers[0];
 
+
         for (int i = 0; i < numbers.length; i++) {
+
             if (numbers[i] < smallest) {
                 smallest = numbers[i];
             }
-        }
 
-        boolean smallestExcluded = false;
+            sum += numbers[i];
 
-        for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] == smallest && !smallestExcluded) {
-                smallestExcluded = true;
-            } else {
-                sum += numbers[i];
-            }
         }
-        System.out.println(sum);
+        for (int element : numbers) {
+            System.out.print(element + " ");
+        }
+        System.out.println();
+        System.out.println("Sum exiting the smallest one:" + (sum - smallest));
     }
 }
